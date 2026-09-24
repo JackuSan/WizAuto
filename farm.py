fb51848373e58a8b32097998bdb86d8232df2d68
JackuSan <jackyckw0713@gmail.com>
2026-09-24 21:38:05 +0800

    v4.4.2 fix2

--- farm.py
+++ farm.py
@@ -2273,7 +2273,7 @@ def Farm_ch4B10F_SnowGlant():
             if find_image("P_Monster_Yeti", similarity=0.75, fail_log=True):
                 for i in range(10):
                     click_image("P_battle_escape", timeout=3)
-                    if wait_image(["P_battle_escape", "P_exit"]) == "P_exit":
+                    if find_image("P_exit"):
                         break
             elif find_image("P_ch4_B10F_SnowGlant", similarity=0.82,fail_log=True):
                 battle_skill([(450, 850, "雪巨人")])
@@ -2286,7 +2286,7 @@ def Farm_ch4B10F_SnowGlant():
             else:
                 for i in range(6):
                     click_image("P_battle_escape", timeout=3)
-                    if wait_image(["P_battle_escape", "P_exit"]) == "P_exit":
+                    if find_image("P_exit"):
                         break
         exitMap(["P_buff", "P_back"])
         core.harken_check("P_village")
